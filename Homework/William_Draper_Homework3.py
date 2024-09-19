456b34d2e53508d3cd91d90ee1243fdb213b681d
Billy <Griffindorv@gmail.com>
2024-09-19 13:43:28 -0400

    Added Extra Problems

--- Homework/William_Draper_Homework3.py
+++ Homework/William_Draper_Homework3.py
@@ -52,19 +52,68 @@ def countDownToFinal(start):
 countDownToFinal(10)
 
 #p10
-def sumOfDivsors(n):
-    sum=0
-    divsor=n
-    while n>0:
-        if(divsor%n==0):
-            sum+=n
+def Buzz(n):
+    for i in range(1,n+1):
+        if(i%3==0):
+            print("Buzz")
+        else:
+            print(i)
+print(Buzz(10))
+#p11
+def negativeSum(aList):
+    suma=0
+    for i in aList:
+        if i<0:
+            suma+=i
+    return suma
+print(negativeSum([-10,20,30,50,-20]))
+#p12`
+def evenPositiveSum(aList):
+    suma=0
+    for i in aList:
+        if i>0 and i%2==0:
+            suma+=i
+#p13
+'''
+Write a function called maximal element(a list) that takes a list as an input.
+The function will return the maximal element in the list. For example
+'''
+def Maximum(aList):
+    maxi=0
+    for i in aList:
+        if(i>maxi):
+            maxi=i
+    return maxi
+#p14
+'''Write a function called is prime(n) that checks whether a number is a prime
+number or not. A prime number is a number whose divisors are 1 and itself. For example:
+2, 5, 7, and 11 are prime numbers while 6 is not.'''
+def isPrime(n):
+    num=n
+
+    while n>1 :
+
+        if num%n==0:
+            return False
         n-=1
-    return sum
+    return True
+print(isPrime(2))
+print(isPrime(6))
+
+#p?
+def sumOfDivsors(n):
+    suma=0
+    divisor=n
+    while n>0:
+        if divisor%n==0:
+            suma+=n
+        n-=1
+    return suma
 
 print(sumOfDivsors(10))
 #p11
 def sumOfSquares(n):
-    if(n==0):
+    if n==0:
         return n
     else :
         return math.sqrt(n)+math.sqrt(sumOfDivsors(n-1))
@@ -72,12 +121,12 @@ def sumOfSquares(n):
 print(sumOfSquares(5))
 #p12
 def isSquare(n):
-    return (math.sqrt(n)>1 and math.sqrt(n)==int(math.sqrt(n)))
+    return math.sqrt(n)>1 and math.sqrt(n)==int(math.sqrt(n))
 
 print(isSquare(36))
 #p13
-def perfectsquare():
+def perfectSquare():
      i=0
      while i<100:
-        if(isSquare(i)):
+        if isSquare(i):
             print(i)
